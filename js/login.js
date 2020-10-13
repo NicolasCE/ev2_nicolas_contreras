6daf40661741d40476b59eee1f9bfd9fe4477840
nicolas contreras <nicontreras569@gmail.com>
2020-10-13 18:22:24 -0500

    avanzando prueba 2

---
+++ js/login.js
@@ -0,0 +1,25 @@
+alert('funciona');
+
+
+export default{
+    
+}
+
+cargar: function() {
+    
+    var resultado = document.getElementById('resultado');
+    var tabla = html`
+    <table>
+        <tr>
+            <th>Alumno</th>
+            <th>Puntaje</th>
+            <th>resultado</th>
+        </tr>
+    </table>
+    
+    `,
+
+}
+agregar: function() {
+
+}
